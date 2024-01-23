60271a1eddc826ec05d626735e73540bc55fe0ac
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-01-23 07:49:14 +0530

    updated about

--- components/About.jsx
+++ components/About.jsx
@@ -10,10 +10,13 @@ const About = () => {
           </h3>
           <h2 className="py-4">Who I am</h2>
           <p className="py-2 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis modi
-            molestiae nihil. Nemo, quod exercitationem excepturi minima est
-            quisquam optio voluptate, nisi omnis autem iure ipsa distinctio
-            quidem eaque modi earum, tenetur laudantium sapiente.
+            I am a recent graduate with a Bachelor's degree in Computer Science
+            and Engineering. My academic background has equipped me with a solid
+            foundation in programming and computer science concepts. I am highly
+            enthusiastic about programming and have a strong passion for solving
+            complex problems through code. I am eager to apply my knowledge and
+            skills to real-world challenges, contribute to innovative projects,
+            and continuously enhance my programming abilities.
           </p>
         </div>
         <div className="'w-full h-auto m-auto shadow-xl shadow-sky-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
